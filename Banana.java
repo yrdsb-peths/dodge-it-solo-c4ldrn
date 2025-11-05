d4813029a092364a0f659f9f18b6300188079211
c4ldrn <348808122@gapps.yrdsb.ca>
2025-11-05 14:49:58 -0500

    added hero to world

--- Banana.java
+++ Banana.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Dude here.
+ * Write a description of class Banana here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Dude extends Actor
+public class Banana extends Actor
 {
     /**
-     * Act - do whatever the Dude wants to do. This method is called whenever
+     * Act - do whatever the Banana wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
--- Hero.java
+++ Hero.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Bomb here.
+ * Write a description of class Hero here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Bomb extends Actor
+public class Hero extends Actor
 {
     /**
-     * Act - do whatever the Bomb wants to do. This method is called whenever
+     * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
